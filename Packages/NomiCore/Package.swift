5adae737cccdf3ca5fd95bc1ba0e1ed234873010
shivams07 <ss07052000@gmail.com>
2026-08-26 16:38:31 +0530

    fix: bump test-host macOS platform to .v15 for unmanaged SwiftData model support (iOS deployment target stays 17.0; unaffected)

--- Packages/NomiCore/Package.swift
+++ Packages/NomiCore/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "NomiCore",
-  platforms: [.iOS(.v17), .macOS(.v14)],
+  platforms: [.iOS(.v17), .macOS(.v15)],
   products: [
     .library(name: "NomiCore", targets: ["NomiCore"])
   ],
--- Packages/NomiPreview/Package.swift
+++ Packages/NomiPreview/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "NomiPreview",
-  platforms: [.iOS(.v17), .macOS(.v14)],
+  platforms: [.iOS(.v17), .macOS(.v15)],
   products: [
     .library(name: "NomiPreview", targets: ["NomiPreview"])
   ],
